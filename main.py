c73f5ac5965e896ac33eda73a87e668d1de94879
Twinkle Gupta <gtwinkle132@gmail.com>
2021-06-25 14:37:58 +0000

    created message event

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ import discord
 #connetion to client
 client  = discord.Client()
 
-client.run('ODU3NjI0MzkyOTg4MDk4NjMy.YNSS9Q.miEiDGH9olb7_-GhwqtHkEw07CU')
+@client.event
+async def on_ready():
+  print("The bot is online")
 
+@client.event
+async def on_message(msg):
+  if msg.author == client.user:
+    return
+  if str(msg.author.id)=="657440181221195788": #only send hello when you send msg
+    if str(msg.content).lower()=="hello":
+      await msg.channel.send("Hello");
+
+client.run('ODU3NjI0MzkyOTg4MDk4NjMy.YNSS9Q.miEiDGH9olb7_-GhwqtHkEw07CU')
