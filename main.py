4d4411e13fdf8d63a479737cb9ea8ba0254c4b0d
Twinkle Gupta <gtwinkle132@gmail.com>
2021-06-25 21:53:39 +0000

    added on_reaction_add event

--- main.py
+++ main.py
@@ -20,7 +20,15 @@ async def on_message(msg):
     return
   if str(msg.author.id)=="657440181221195788": #only send hello when you send msg
     if str(msg.content).lower()=="hello":
-      await msg.channel.send("Hello");
+      await msg.channel.send("Hello")
+  if str(msg.content).lower() == "hi mr.bot":
+      await msg.add_reaction("\U0001F91D")
+
+
+@client.event
+async def on_reaction_add(reaction,user):
+  await reaction.message.channel.send(str(user)+" reacted with "+reaction.emoji)
+
 
 
 client.run(my_secret)
